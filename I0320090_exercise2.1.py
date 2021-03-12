279788874018e0a4325b4dbf7696488d1f976f43
Salma Ardelia Irfani <Salma Ardelia Irfani>
2021-03-11 19:12:32 -0800

    Tugas 2

---
+++ I0320090_exercise2.1.py
@@ -0,0 +1,18 @@
+#tipe data Boolean
+print(True)
+
+#tipe data string
+print("Ayo belajar Python")
+print('Belajar Python sangat mudah')
+
+#tipe data Integer
+print(20)
+
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimal
+print(9a)
+
+#tipe data Complex
+print(5j)
