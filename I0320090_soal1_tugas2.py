8d3ba35968083fc871723b60d55c514a96f4a3ea
Salma Ardelia Irfani <Salma Ardelia Irfani>
2021-03-12 00:13:02 -0800

    Tugas 2

---
+++ I0320090_soal1_tugas2.py
@@ -0,0 +1,33 @@
+print("Menghitung luas persegi panjang")
+Panjang = 15
+Lebar = 5
+Luas_persegipanjang = Panjang * Lebar
+print("Panjang persegi panjang: ", Panjang)
+print("Lebar persegi panjang: ", Lebar)
+print("Luas persegi panjang : ", Luas_persegipanjang)
+
+print("Menghitung luas lingkaran")
+r = 7
+Luas_lingkaran = 3.14 * (r ** 2)
+print("Jari-jari lingkaran : ", r)
+print("Luas lingkaran : ", Luas_lingkaran)
+
+print("Menghitung luas kubus")
+Panjang_rusuk = 5
+Jumlah_sisi = 6
+Luas_kubus = Jumlah_sisi * (Panjang_rusuk ** 2)
+print("Panjang rusuk kubus : ", Panjang_rusuk)
+print("Jumlah sisi kubus : ", Jumlah_sisi)
+print("Luas kubus : ", Luas_kubus)
+
+print("Menghitung konversi suhu celcius ke fahrenheit")
+Celcius = 10
+Fahrenheit = ((9/5) * 10) + 32
+print("Suhu : ", Celcius, "C")
+print("Suhu 10 Celcius : ", Fahrenheit, "Fahrenheit")
+
+print("Menghitung konversi suhu reamur ke kelvin")
+reamur = 20
+kelvin = ((5/4) * 20) +273
+print("Suhu : ", reamur, "R")
+print("Suhu 20 Reamur :", kelvin, "Kelvin")
---
+++ I0320090_soal2_tugas2.py
@@ -0,0 +1,38 @@
+#Biodata
+
+Nama_lengkap = "Salma Ardelia Irfani"
+Nama_panggilan = "Salma"
+Tempat_lahir = "Karanganyar"
+Tanggal_lahir = "24 Agustus 2001"
+Jenis_kelamin = "perempuan"
+Umur = "19 tahun"
+Alamat = "Jalan Solo-Sragen Km.10, Dawung RT.03 RW 01, Kemiri, Kebakkramat, Karangnyar"
+Agama = "islam"
+Kewarganegaraan = "Indonesia"
+Anak_ke = "satu dari dua bersaudara"
+Adik = "Melly Syafika Putri"
+Hobi = "fotografi dan jalan-jalan"
+Golongan_darah = "O"
+Tinggi_badan = 159
+Berat_badan = 70
+Ukuran_sepatu = 38
+Jurusan = "Teknik Industri"
+Perguruantinggi = "Universitas Sebelas Maret Surakarta"
+Cita_cita = "pengusaha"
+Email = "salmaardelia24@gmail.com"
+
+print("Perkenalkan nama saya", Nama_lengkap,"," "biasa dipanggil", Nama_panggilan)
+print("Saya lahir di Kabupaten", Tempat_lahir, "pada tanggal", Tanggal_lahir)
+print("Saya berjenis kelamin", Jenis_kelamin)
+print("Saya sekarang berumur", Umur, "tahun")
+print("Alamat rumah saya", Alamat)
+print("Saya beragama", Agama)
+print("Kewarganegaraan saya adalah", Kewarganegaraan)
+print("Saya merupakan", Anak_ke, "Adik saya bernama", Adik)
+print("Hobi saya adalah", Hobi)
+print("Golongan darah saya adalah", Golongan_darah)
+print("Tinggi badan saya", Tinggi_badan, "cm", "sedangkan berat badan saya", Berat_badan, "kg")
+print("Ukuran sepatu saya", Ukuran_sepatu, "cm")
+print("Saya sekarang menjadi mahasiswa di jurusan", Jurusan, "di", Perguruantinggi)
+print("Cita-cita saya menjadi seorang", Cita_cita)
+print("Email saya yaitu", Email)
